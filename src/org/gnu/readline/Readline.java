6be0526a44559487b2df5628bfd813231d16a3e1
Henner Zeller <h.zeller@acm.org>
2002-03-03 10:47:47 +0000

    - added method hasTerminal() - added method hasTerminalImpl()

--- src/org/gnu/readline/Readline.java
+++ src/org/gnu/readline/Readline.java
@@ -284,6 +284,22 @@ public class Readline {
   /////////////////////////////////////////////////////////////////////////////
 
   /**
+   *  return if we have a terminal. This requires, that any of the native
+   *  libraries have been loaded yet
+   *  (so call Readline.{@link load(ReadlineLibrary)}())
+   *  first, otherwise this will always return true.
+   */
+
+   public static boolean hasTerminal() {
+     if (iLib != ReadlineLibrary.PureJava) {
+       return hasTerminalImpl();
+     }
+     return true;
+   }
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
      Query word break characters.
   */
     
@@ -374,6 +390,14 @@ public class Readline {
   */
 
   private native static void cleanupReadlineImpl();
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+     native implementation of isatty();
+  */
+
+  private native static boolean hasTerminalImpl();
 
   /////////////////////////////////////////////////////////////////////////////
 
